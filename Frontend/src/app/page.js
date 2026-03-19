6217f510be01fe276f02f42025690db1764812d8
santosh majakoti <santoshjr08@gmail.com>
2026-03-19 21:55:52 +0545

    Refactor global styles and update page component structure

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      frontend
+    <div className='text-primary'>
+     <p className='bg-secondary'>what</p>
     </div>
   )
 }
