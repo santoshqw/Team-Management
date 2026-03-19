222f923150ecfc831185f9e3ed0ef46a11c4e547
santosh majakoti <santoshjr08@gmail.com>
2026-03-19 22:22:49 +0545

    Refactor color variables in globals.css and update button class in page.js

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div className='text-primary'>
      <p className='bg-secondary'>what</p>
+     <button className='text-success'>click me</button>
     </div>
   )
 }
